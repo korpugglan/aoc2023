72fd2b84477ea057274b886841cf985184c62ef0
korpugglan <korpugglan@hotmail.com>
2023-12-25 15:51:01 +0100

    Refactor

--- 03/03.py
+++ 03/03.py
@@ -22,79 +22,72 @@ def print_line(print_chars="-", repetition=100):
     return
 
 
+def update_total_score_and_reset_digit_data():
+    global total_score
+    global digit_string
+    global position_list
+
+    total_score += int(digit_string)
+    digit_string = ""
+    position_list = []
+
+    return
+
 # Load input data
 day = "03"
 input_file = open(f"{day}/input_{day}.txt")
 input_list = input_file.read().splitlines()
 
-# Total score
+# Initial variables
 total_score = 0
+digit_string = ""
+position_list = []
 max_row_nr = len(input_list) - 1
+# Loop through all the input rows
 for row_nr, input_row in enumerate(input_list):
-    print_line("=", repetition=144)
-    print(input_row)
-
-    digit_string = ""
-    position_list = []
+    # Define the last position for this row
     max_position = len(input_row) - 1
-
+    # Loop through all the characters in the input row
     for position, character in enumerate(input_row):
-        # If digit, add it to string and append position
+        # If it is a digit, add it to a digit string and append its position to a list
         if character.isdigit():
             digit_string += character
             position_list.append(position)
 
-        # If a digit string has been created and (character is not a digit or is a digit on the last position)
+        # If a digit string exists and the current character is either not a digit or in the last position in the string
         if digit_string != "" and (not character.isdigit() or position == max_position):
-            print(f"Digit string: {digit_string}")
             # Define horizontal check positions
-            left_check_position = min(position_list) - 1
-            right_check_position = max(position_list) + 1
+            # Setting to 0 prevents indexing issues and has a check performed on a digit in the worst case
+            if min(position_list) - 1 < 0:
+                left_check_position = 0
+            else:
+                left_check_position = min(position_list) - 1
+            if max(position_list) + 1 > max_position:
+                right_check_position = max_position
+            else:
+                right_check_position = max(position_list) + 1
 
             # Check left
-            if left_check_position >= 0:
-                if check_for_symbol(input_row, left_check_position, left_check_position + 1):
-                    print("Found one on the left")
-                    total_score += int(digit_string)
-                    digit_string = ""
-                    position_list = []
-                    continue
+            if check_for_symbol(input_row, left_check_position, left_check_position + 1):
+                update_total_score_and_reset_digit_data()
+                continue
             # Check right
-            if right_check_position <= max_position:
-                if check_for_symbol(input_row, right_check_position, right_check_position + 1):
-                    print("Found one on the right")
-                    total_score += int(digit_string)
-                    digit_string = ""
-                    position_list = []
-                    continue
-
-            # Fix for ranges
-            if left_check_position < 0:
-                left_check_position = 0
-            if right_check_position >= max_position:
-                right_check_position = max_position
-
+            if check_for_symbol(input_row, right_check_position, right_check_position + 1):
+                update_total_score_and_reset_digit_data()
+                continue
             # Check above
-            if row_nr > 0:
-                if check_for_symbol(input_list[row_nr - 1], left_check_position, right_check_position + 1):
-                    print("Found one above")
-                    total_score += int(digit_string)
-                    digit_string = ""
-                    position_list = []
-                    continue
+            if row_nr > 0 and \
+               check_for_symbol(input_list[row_nr - 1], left_check_position, right_check_position + 1):
+                update_total_score_and_reset_digit_data()
+                continue
             # Check below
-            if row_nr < max_row_nr:
-                if check_for_symbol(input_list[row_nr + 1], left_check_position, right_check_position + 1):
-                    print("Found one below")
-                    total_score += int(digit_string)
-                    digit_string = ""
-                    position_list = []
-                    continue
-            # print(digit_string)
-            # print(position_list)
+            if row_nr < max_row_nr and \
+               check_for_symbol(input_list[row_nr + 1], left_check_position, right_check_position + 1):
+                update_total_score_and_reset_digit_data()
+                continue
 
             digit_string = ""
             position_list = []
 
-    print(f"Total score: {total_score}")  # 533287 too low
+print(f"Total score: {total_score}")  # 533775
 

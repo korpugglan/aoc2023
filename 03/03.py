70f881e93255e9ab143eee638ca3293b9df9bcd1
korpugglan <korpugglan@hotmail.com>
2023-12-25 19:02:06 +0100

    Start on part 2

--- 03/03.py
+++ 03/03.py
@@ -1,4 +1,4 @@
-# Day 03
+# Day 03 (VERY UGLY CODE AND SOLUTION)
 # Define functions
 def check_for_symbol(input_string, range_start, range_end):
     check_string = input_string[range_start:range_end]
@@ -8,6 +8,22 @@ def check_for_symbol(input_string, range_start, range_end):
         else:
             return True
     return False
+
+
+def complete_number(row, start_index, max_index):
+    number = row[start_index]
+
+    left_index = start_index - 1
+    while left_index >= 0 and row[left_index].isdigit():
+        number = row[left_index] + number
+        left_index -= 1
+
+    right_index = start_index + 1
+    while right_index <= max_index and row[right_index].isdigit():
+        number = number + row[right_index]
+        right_index += 1
+
+    return int(number)
 
 
 def print_line(print_chars="-", repetition=100):
@@ -33,11 +49,13 @@ def update_total_score_and_reset_digit_data():
 
     return
 
+
 # Load input data
 day = "03"
 input_file = open(f"{day}/input_{day}.txt")
 input_list = input_file.read().splitlines()
 
+# Part 01
 # Initial variables
 total_score = 0
 digit_string = ""
@@ -91,3 +109,74 @@ for row_nr, input_row in enumerate(input_list):
 
 print(f"Total score: {total_score}")  # 533775
 
+# Part 02
+# Initial variables
+new_total_score = 0
+max_row_nr = len(input_list) - 1
+# Loop through all the input rows
+for row_nr, input_row in enumerate(input_list[:5]):
+    max_position = len(input_row) - 1
+    # Loop through the row string and find *
+    for position, character in enumerate(input_row):
+        if character == "*":
+            # Create a new empty numbers list for this particular *
+            numbers_list = []
+
+            # Define horizontal check positions
+            # Setting to 0 prevents indexing issues and has a check performed on a digit in the worst case
+            if position - 1 < 0:
+                left_check_position = 0
+            else:
+                left_check_position = position - 1
+            if position + 1 > max_position:
+                right_check_position = max_position
+            else:
+                right_check_position = position + 1
+
+            # Keep checking for adjacent number until it finds 2 of them
+            # Check above
+            if row_nr > 0:
+                # Get the values to check for above
+                check_range = input_list[row_nr - 1][left_check_position:right_check_position + 1]
+                # If the digits are split, there are two numbers
+                if check_range[0].isdigit() and not check_range[1].isdigit() and check_range[2].isdigit():
+                    numbers_list.append(complete_number(input_list[row_nr - 1], left_check_position, max_position))
+                    numbers_list.append(complete_number(input_list[row_nr - 1], right_check_position, max_position))
+                # Otherwise, if any digit is found, add the single number
+                elif any(x.isdigit() for x in check_range):
+                    for nr, x in enumerate(check_range):
+                        if x.isdigit():
+                            numbers_list.append(complete_number(input_list[row_nr - 1],
+                                                                left_check_position + nr,
+                                                                max_position))
+                            break
+
+            # Check horizontal
+            if input_row[left_check_position].isdigit():
+                numbers_list.append(complete_number(input_row, left_check_position, max_position))
+            if input_row[right_check_position].isdigit():
+                numbers_list.append(complete_number(input_row, right_check_position, max_position))
+
+            # Check below
+            if row_nr < max_row_nr:
+                # Get the values to check for below
+                check_range = input_list[row_nr + 1][left_check_position:right_check_position + 1]
+                # If the digits are split, there are two numbers
+                if check_range[0].isdigit() and not check_range[1].isdigit() and check_range[2].isdigit():
+                    numbers_list.append(complete_number(input_list[row_nr + 1], left_check_position, max_position))
+                    numbers_list.append(complete_number(input_list[row_nr + 1], right_check_position, max_position))
+                # Otherwise, if any digit is found, add the single number
+                elif any(x.isdigit() for x in check_range):
+                    for nr, x in enumerate(check_range):
+                        if x.isdigit():
+                            numbers_list.append(complete_number(input_list[row_nr + 1],
+                                                                left_check_position + nr,
+                                                                max_position))
+                            break
+
+            # Add the factor of the numbers to the total score if two have been found
+            if len(numbers_list) == 2:
+                new_total_score += (numbers_list[0] * numbers_list[1])
+            numbers_list = []
+
+print(f"New total score: {new_total_score}")  # 799971216331 too high, 2221562 too low

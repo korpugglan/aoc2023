43a516d3e70016caca669aa9cc58ea327095fab4
korpugglan <korpugglan@hotmail.com>
2024-02-11 01:34:47 +0100

    Add base files for day 05

---
+++ 05/05.py
@@ -0,0 +1,18 @@
+# Day 05
+# Define functions
+def print_line(print_chars="-", repetition=100):
+    """Prints an amount of strings in a row.
+       Args:
+           print_chars (str): The string to print repeatedly.
+           repetition (int): The amount of times the string is printed.
+       Returns:
+           None
+    """
+    print(print_chars * repetition)
+    return
+
+
+# Load input data
+day = "05"
+input_file = open(f"{day}/input_{day}.txt")
+input_list = input_file.read().splitlines()
